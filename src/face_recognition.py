1974df45230bb6d29ddb5ff04051040d1ab2ba25
Szymon Skrzypczyk <77851583+SzymonSkrzypczyk@users.noreply.github.com>
2023-09-30 15:58:10 +0200

    wykrywanie twarzy ogarniete

--- src/face_recognition.py
+++ src/face_recognition.py
@@ -1,20 +1,42 @@
+from time import sleep
 from pathlib import Path
-# from mtcnn.mtcnn import MTCNN
+from mtcnn.mtcnn import MTCNN
 from PIL import Image
 from numpy import asarray
 import cv2
 from scipy.spatial.distance import cosine
-# from keras_vggface.vggface import VGGFace
-# from keras_vggface.utils import preprocess_input
+from keras_vggface.vggface import VGGFace
+from keras_vggface.utils import preprocess_input
 
 THRESHOLD = 0.5
 
 
+class FaceNotDetected(ValueError):
+    pass
+
+
+def add_face(image: Image):
+    max_index = max([int(x.with_suffix("").name) for x in (Path(__file__).parent.parent / 'assets').iterdir()])
+    target_path = Path(__file__).parent.parent / 'assets' / f'{max_index + 1}.jpg'
+    image.save(str(target_path))
+
+
+def check_saved(new_face: Image):
+    images = Path(__file__).parent.parent / 'assets'
+    for i in images.iterdir():
+        if evaluate(new_face, Image.open(str(i))):
+            return i.with_suffix("").name
+    return None
+
+
 def get_image():
     cam = cv2.VideoCapture(0)
-    res, frame = cam.read()
-    while frame is None and res:
-        res, frame = cam.read()
+    for i in range(5):
+        _, frame = cam.read()
+
+    cam.release()
+    cv2.destroyAllWindows()
+
     return Image.fromarray(frame)
 
 
@@ -23,6 +45,8 @@ def extract_face(image):
     detector = MTCNN()
     res = detector.detect_faces(image_array)
     # x2 and y2 as width and height respectively
+    if not res:
+        raise FaceNotDetected("There has been an error with your face :(") from None
     x1, y1, x2, y2 = res[0]['box']
     # bottom corner
     x2 += x1
@@ -43,4 +67,5 @@ def evaluate(image1: Image, image2: Image, threshold: int = THRESHOLD):
 
 
 if __name__ == '__main__':
-    evaluate(Image.open(str(Path(__file__).parent / 'assets' / 'test1.jpg')), get_image())
+    # print(evaluate(Image.open(str(Path(__file__).parent.parent / 'assets' / '1.jpg')), get_image()))
+    print(check_saved(get_image()))
