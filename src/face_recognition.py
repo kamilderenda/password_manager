93925d83fac1193fa2588bcff7ec54fe69fd0da4
jasun39 <jasun0309@gmail.com>
2023-09-30 19:31:51 +0200

    decomentacja face_recognition.py

--- src/face_recognition.py
+++ src/face_recognition.py
@@ -25,7 +25,8 @@ class FaceNotDetected(ValueError):
 def create_model() -> Model:
     """
     Creates model used to recognise faces
-    :return:
+    :returns: Model used to recognise faces
+    :rtype: Model
     """
     return VGGFace(include_top=False, model='resnet50', input_shape=(224, 224, 3), pooling='avg')
 
@@ -34,7 +35,9 @@ def add_face(image: Image) -> None:
     """
     Adds face to the face directory with an index of the added user
     :param image:
-    :return:
+    :type image: Image
+    :return: None
+    :rtype: NoneType
     """
     max_index = max([int(x.with_suffix("").name) for x in (Path(__file__).parent.parent / 'assets').iterdir()])
     target_path = Path(__file__).parent.parent / 'assets' / f'{max_index + 1}.jpg'
@@ -45,10 +48,14 @@ def check_saved(new_face: Image, image: Path, model: Model) -> Union[int, None]:
     """
     Checks all the saved users and returns a user's index if evaluation is True.
     If none of the evaluations is true returns None
-    :param image:
-    :param model:
     :param new_face:
-    :return:
+    :type new_face: Image
+    :param image:
+    :type image: Path
+    :param model:
+    :type model: Model used to recognise faces
+    :return: Union
+    :rtype: list[int, None]
     """
     # images = Path(__file__).parent.parent / 'assets'
     if evaluate(new_face, Image.open(image), model):
@@ -60,8 +67,11 @@ def predict(camera_image: Image, model: Model) -> Union[int, None]:
     """
     Predicts the id of the user trying to log in, if not detected return None # and create entry
     :param camera_image:
+    :type camera_image: Image
     :param model:
-    :return:
+    :type model: Model used to recognise faces
+    :return: Union
+    :rtype: list[int, None]
     """
     # to ma byc przyspieszone stad tez postac funckji
     camera_image.show()
@@ -76,7 +86,8 @@ def predict(camera_image: Image, model: Model) -> Union[int, None]:
 def get_image() -> Image:
     """
     Reads user's face from camera, if there's an error throws CameraNotFunctioning error
-    :return:
+    :return: Image of user's face
+    :rtype: Image
     """
     cam = cv2.VideoCapture(0)
     for i in range(5):
@@ -93,8 +104,10 @@ def get_image() -> Image:
 def extract_face(image) -> np.array:
     """
     Extracts faces from the image
-    :param image:
-    :return:
+    :param image: Image to exctract
+    :type image: Image
+    :return: Extracted face scaled down
+    :rtype: np. Array
     """
     image_array = np.asarray(image)
     detector = MTCNN()
@@ -113,11 +126,16 @@ def extract_face(image) -> np.array:
 def evaluate(image1: Image, image2: Image, model: Model, threshold: int = THRESHOLD) -> bool:
     """
     Evaluates whether the given images represent the same person
-    :param model:
-    :param image1:
-    :param image2:
+    :param image1: Image of logging person
+    :type image1: Image
+    :param image2: Image for comparison
+    :type image2: Image
+    :param model: Model used to recognise faces
+    :type model: Model
     :param threshold:
-    :return:
+    :type threshold: int
+    :return: Bool type whether the given image matches user face
+    :rtype: Bool
     """
     faces = [extract_face(image1), extract_face(image2)]
     samples = preprocess_input(np.asarray(faces, 'float32'))
